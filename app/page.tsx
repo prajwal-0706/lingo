7676a28c61b3d1ed6780a365db4ee068b9b7b23b
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-03-18 21:25:19 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,7 @@ export default function Home() {
     <main>
       <div>
         <div>
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a>
             By{' '}
             <Image
               src="/vercel.svg"
