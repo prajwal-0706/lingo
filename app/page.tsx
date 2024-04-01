109609dbe12b943cfc34e69d113c1c18e70e1609
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-01 21:35:09 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         <a>
           <h2>
-            <span>-&gt;</span>
+            <span></span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Learn about Next.js in an interactive course with&nbsp;quizzes!
