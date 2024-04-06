58c67653ac0b1de5960e590e9ce996eb33ba3ee8
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-06 21:38:40 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,6 @@ export default function Home() {
     <main>
       <div>
         <a>
-          <h2 className="mb-3 text-2">
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Explore starter templates for Next.js.
           </p>
