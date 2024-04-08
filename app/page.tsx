fc9cc4fb1263f1b236075396dcac1c042e5589d2
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-08 21:39:33 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main>
       <div>
-        <a>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
