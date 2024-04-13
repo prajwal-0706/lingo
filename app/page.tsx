a7ccbcdebc09ce16bc3837f39e0f1192a699baa6
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-13 21:41:39 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main>
       <div>
         <a>
-          <h2 className="mb-3 text-2xl font-semibold"></h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50"></p>
         </a>
       </div>
