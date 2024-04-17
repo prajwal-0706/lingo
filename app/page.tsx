01c9ab88c9398b6ed67cb17b69ba027797317c1b
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-17 21:43:57 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="">
+      <div className="">Hello</div>
+    </div>
+  );
 }
