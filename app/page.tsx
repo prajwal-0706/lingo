ee5b4ac419fe58ce2bcfadf0f75cb286909984c1
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-18 21:36:39 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main>
       <div>
-        <a>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50"></p>
-        </a>
-
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
