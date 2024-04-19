2847c45ef5609e3811fd9a9ee3f060bd96ca73e3
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-19 21:45:39 +0530

    feat: Added ShadCn

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="">
-      <div className="">Hello</div>
+      <div className="">Hello World</div>
     </div>
   );
 }
