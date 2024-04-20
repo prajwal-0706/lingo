3385e1d48b943cfabd79fe48d75b0c0126bcc795
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-21 01:49:57 +0530

    fix: Changed Outlined Buttons (Borders)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,16 +13,20 @@ const buttonVariants = cva(
           'bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500',
         primary:
           'bg-sky-400 text-primary-foreground hover:bg-sky-500/90 border-sky-500 border-b-4 active:border-b-0',
-        primaryOutline: 'bg-white text-sky-500 hover:bg-slate-100',
+        primaryOutline:
+          'bg-white text-sky-500 hover:bg-slate-100 border-slate-400/50 border-b-4 active:border-b-0',
         secondary:
           'bg-green-400 text-primary-foreground hover:bg-green-500/90 border-green-500 border-b-4 active:border-b-0',
-        secondaryOutline: 'bg-white text-green-500 hover:bg-slate-100',
+        secondaryOutline:
+          'bg-white text-green-500 hover:bg-slate-100 border-slate-400/50 border-b-4 active:border-b-0',
         danger:
           'bg-rose-400 text-primary-foreground hover:bg-rose-500/90 border-rose-500 border-b-4 active:border-b-0',
-        dangerOutline: 'bg-white text-rose-500 hover:bg-slate-100',
+        dangerOutline:
+          'bg-white text-rose-500 hover:bg-slate-100 border-slate-400/50 border-b-4 active:border-b-0',
         super:
           'bg-indigo-400 text-primary-foreground hover:bg-indigo-500/90 border-indigo-500 border-b-4 active:border-b-0',
-        superOutline: 'bg-white text-indigo-500 hover:bg-slate-100',
+        superOutline:
+          'bg-white text-indigo-500 hover:bg-slate-100 border-slate-400/50 border-b-4 active:border-b-0',
         ghost:
           'bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100',
         sidebar:
