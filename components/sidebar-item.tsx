2437ca5d46d885f4f1c2c73e4ad1e2b166cb51a2
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-04-25 11:01:55 +0530

    feat: Side Items and Images Added

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+
+import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type SideBarItemProps = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export default function SidebarItem({
+  label,
+  iconSrc,
+  href,
+}: SideBarItemProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Button
+      asChild
+      className="justify-start h-[52px]"
+      variant={isActive ? 'sidebarOutline' : 'sidebar'}
+    >
+      <Link href={href}>
+        <Image
+          alt={label}
+          className="mr-5"
+          height={28}
+          width={28}
+          src={iconSrc}
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,10 @@
-import { cn } from '@/lib/utils';
 import { ClassValue } from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+import { cn } from '@/lib/utils';
+import SidebarItem from '@/components/sidebar-item';
 
 export function Sidebar({ className }: { className?: ClassValue }) {
   return (
@@ -10,7 +14,17 @@ export function Sidebar({ className }: { className?: ClassValue }) {
         className
       )}
     >
-      shdfgksdhjfgkshjdfg
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image alt="lingo logo" height={40} width={40} src="/mascot.svg" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2">
+        <SidebarItem iconSrc="/learn.svg" href="/learn" label="learn" />
+      </div>
     </div>
   );
 }
