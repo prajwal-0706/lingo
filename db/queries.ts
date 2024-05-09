8d68feb77981c4749c0671b667f10b8e2ea88770
prajwal-0706 <prajwalgadhave356@gmail.com>
2024-05-10 02:31:00 +0530

    feat: Updated Seeding Script of Neon and Implemented Normalization for get Units Query

--- db/queries.ts
+++ db/queries.ts
@@ -4,7 +4,7 @@ import { cache } from 'react';
 import db from './drizzle';
 import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
-import { courses, userProgress } from '@/db/schema';
+import { courses, units, userProgress } from '@/db/schema';
 
 export const getCourses = cache(async () => await db.query.courses.findMany());
 
@@ -26,4 +26,45 @@ export const getCourseById = cache(async (courseId: number) => {
     where: eq(courses.id, courseId),
     // TODO: Populate units and lessons
   });
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress?.activeCourseId) return [];
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every(
+            (challengeProgress) => challengeProgress.completed
+          )
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
 });
--- db/schema.ts
+++ db/schema.ts
@@ -36,7 +36,7 @@ export const unitsRelations = relations(units, ({ one, many }) => ({
     fields: [units.courseId],
     references: [courses.id],
   }),
-  lesson: many(lessons),
+  lessons: many(lessons),
 }));
 
 export const lessons = pgTable('lessons', {
@@ -77,7 +77,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
   challengeProgress: many(challengeProgress),
 }));
 
-export const challengeOptions = pgTable('challengeOptions', {
+export const challengeOptions = pgTable('challenge_options', {
   id: serial('id').primaryKey(),
   challengeId: integer('challenge_id')
     .references(() => challenges.id, { onDelete: 'cascade' })
@@ -98,7 +98,7 @@ export const challengeOptionsRelations = relations(
   })
 );
 
-export const challengeProgress = pgTable('challengeProgress', {
+export const challengeProgress = pgTable('challenge_progress', {
   id: serial('id').primaryKey(),
   userId: text('user_id').notNull(), // TODO: Confirm that this doesnt break
   challengeId: integer('challenge_id')
